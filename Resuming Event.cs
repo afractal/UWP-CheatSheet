a60c31afaea7e3f5e5e4d7f078fe3e0b18495485
hermesxgjini <hermesxgjini@users.noreply.github.com>
2014-12-27 23:01:49 +0100

    Update and rename Resuming Event to Resuming Event.cs

--- Resuming Event.cs
+++ Resuming Event.cs
@@ -1,7 +1,8 @@
 // This must be coded in App.cs
 
 // From the Suspended state, your app can assume the NotRunning and Running states.
-// You don’t have control over the transition to the NotRunning state, but you can control the behavior when passed to the Running state by firing the Resuming event.
+// You don’t have control over the transition to the NotRunning state, but you can control the behavior when passed 
+// to the Running state by firing the Resuming event.
 // C# Managing the Resuming event is really easy. The event handler can be implemented as follows in App.xaml.cs: 
 
 void OnResuming(object sender, object e) 
