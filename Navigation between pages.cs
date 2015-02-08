9765a651e423d6cc1c274da6f8529ca2fe6009f3
hermesxgjini <hermesxgjini@users.noreply.github.com>
2015-02-08 20:28:10 +0100

    Update Navigation between pages.cs

--- Navigation between pages.cs
+++ Navigation between pages.cs
@@ -15,3 +15,12 @@ private void itemListView_ItemClick(object sender, ItemClickEventArgs e)
         throw new Exception(this.resourceLoader.GetString("NavigationFailedExceptionMessage"));
     }
 }
+
+// the most generic version is by using it with an botton click event 
+// here we navigate to a new page called SecondPage by clicking the botton
+// for good visual effects make sure you use Page Transitions (everybody loves eye candy)
+
+private void Button_Click(object sender, RoutedEventArgs e)
+{
+    Frame.Navigate(typeof(SecondPage) );
+}
