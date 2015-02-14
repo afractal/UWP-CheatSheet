be48c72306c0df395425c18da50b53ef7e1cd95f
hermesxgjini <hermesxgjini@users.noreply.github.com>
2015-02-14 22:32:56 +0100

    Create Feedback Shake feature.cs

---
+++ Feedback Shake feature.cs
@@ -0,0 +1,31 @@
+// Add a reference to the ShakGesturesLibraryRuntime which is the new version that supports winRT 8.1
+// you can do that by clicking on the right mouse and choose Manage NuGet Packages, after that write Shake Gesture 
+// on the searchbox and click ok, install it and it is done.
+
+namespace ThirdTest
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            ShakeGesturesHelper.Instance.ShakeGesture += Instance_ShakeGesture;
+            ShakeGesturesHelper.Instance.MinimumRequiredMovesForShake = 5;
+            ShakeGesturesHelper.Instance.Active = true;
+        }
+
+        async void Instance_ShakeGesture(object sender, ShakeGestureEventArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Frame.Navigate(typeof(TheShakenPage) );
+            });
+        }
+        
+        protected override void OnNavigatedTo(NavigationEventArgs e){ }
+    }
+
+// namespace ends here
+}
