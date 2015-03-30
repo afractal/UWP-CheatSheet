9a971f94d72e903c2a6525578b46b650023a4bd8
hermesxgjini <hermesxgjini@users.noreply.github.com>
2015-03-31 01:25:53 +0200

    Create OnActivated.cs

---
+++ OnActivated.cs
@@ -0,0 +1,39 @@
+ protected override void OnActivated(IActivatedEventArgs args)
+ {
+    switch (args.Kind)    
+    {
+        case ActivationKind.CachedFileUpdater:  
+            //Do something here           
+            break;      
+        case ActivationKind.CameraSettings:       
+            //Do something here        
+            break;     
+        case ActivationKind.ContactPicker:        
+            //Do something here       
+            break;    
+        case ActivationKind.Device:      
+            //Do something here      
+            break;   
+        case ActivationKind.File:    
+            //Do something here      
+            break;
+        case ActivationKind.FileOpenPicker: 
+            //Do something here    
+            break;  
+        case ActivationKind.FileSavePicker:       
+            //Do something here        
+            break;        
+        case ActivationKind.PrintTaskSettings:   
+            //Do something here      
+            break;     
+        case ActivationKind.Protocol:   
+            //Do something here       
+            break;    
+        case ActivationKind.Search:   
+            //Do something here       
+            break;     
+        case ActivationKind.ShareTarget:      
+            //Do something here        
+            break;  
+    }
+} 
