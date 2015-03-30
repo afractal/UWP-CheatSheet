a0c52d3566407f308623767e44c4a26c3dfcaa7b
hermesxgjini <hermesxgjini@users.noreply.github.com>
2015-03-31 01:31:52 +0200

    Update Notifications.cs

--- Notifications.cs
+++ Notifications.cs
@@ -99,3 +99,23 @@ void gotNotification(PushNitificationChannel sender, PushNotificationReceivedEve
 // Use Azure Notification Hub
 // Cross device notifications
 // http://aka.ms/notifyhub
+
+
+//--> Send TileNitification
+private void SendTileNotification()
+{
+  // get an XML DOM varsion of a specific template by using getTemplateContext
+  var tileXml = TileUpdateManger.GetTemplateContent(TileTemplate.TileSquare);
+  
+  //you will need tolock at the template documentation to know how many text field a paricular template has
+  // get the text attribute for this template and fill it in
+  var tileAttributes = tileXml.GetElementByTagName("Hello World");
+  
+  // create the notificatiob from the XML
+  var tileNotification = new TileNotification(tileXml);
+  
+  // send the notification to the calling apps tile
+  TileUpdateManager.CreateTileUpdaterForApplication().Update(tileNotification);
+}
+
+// for more information write tile notification template on your favorite webbrowser
