76ec78023c15914a8679435f71306d533ff4b1b0
hermesxgjini <hermesxgjini@users.noreply.github.com>
2015-03-31 17:44:41 +0200

    Update Data Storage.cs

--- Data Storage.cs
+++ Data Storage.cs
@@ -59,11 +59,11 @@ Object value= localSettings.Values["exampleSettings"];
 
 if(value==null)
 {
-  // No data
+    // No data
 }
 else
 {
-  //Access data in value
+    //Access data in value
 }
 
 // Delete a simple settings
@@ -73,8 +73,8 @@ localSetting.Values.Remove("exampleSettings");
 
 private void name_TextChange(object sender, TextChangedEventArgs e)
 {
-  Windows.Storage.ApplicationDataContainer roamingSettings= Windows.Storage.ApplicationDAta.Current.RoamingSettings;
-  roamingSettings.Values["userName"]= name.Text;
+    Windows.Storage.ApplicationDataContainer roamingSettings= Windows.Storage.ApplicationDAta.Current.RoamingSettings;
+    roamingSettings.Values["userName"]= name.Text;
 }
 
 // The Roaming Settings are exposed as a dectionary into which an application can save data
@@ -86,7 +86,7 @@ Windows.Storage.ApplicationDataContainer roamingSettings= Windows.Storage.Applic
 
 if(roamingSettings.Values.ContainsKey("userName"))
 {
-  name.Text= roamingSettings.Values["userName"].ToString();
+    name.Text= roamingSettings.Values["userName"].ToString();
 }
 // This code queries the roaming settings by using the key name
 // The name will be reflected across multiple devices (including Windows 8.1 ones) 
@@ -96,22 +96,22 @@ if(roamingSettings.Values.ContainsKey("userName"))
 // onNavigatedTo() method , write it on github
 namespace AppDataTest
 {
-  public sealed partial class MainPage : Page
-  {
-    public MainPage()
+    public sealed partial class MainPage : Page
     {
-      this.InitializeComponent();
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var folder = ApplicationData.Current.LocalFolder;
+            var subfolder = await folder.CreateFolderAsync("MyFolder", CreationCollisionOption.OpenIfExists);
+            var result = await subfolder.CreateFileAsync("MyFile.txt", CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(result, "lolz");
+            var content = await FileIO.ReadTextAsync(result);
+            myTextBlock.Text = content.ToString();
+        }
     }
-    protected async override void OnNavigatedTo(NavigationEventArgs e)
-    {
-      var folder = ApplicationData.Current.LocalFolder;
-      var subfolder = await folder.CreateFolderAsync("MyFolder", CreationCollisionOption.OpenIfExists);
-      var result = await subfolder.CreateFileAsync("MyFile.txt", CreationCollisionOption.ReplaceExisting);
-      await FileIO.WriteTextAsync(result, "lolz");
-      var content = await FileIO.ReadTextAsync(result);
-      myTextBlock.Text = content.ToString();
-    }
-  }
 
 // namespace ends here
 }
