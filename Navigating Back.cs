62360aba84cebd3933378d31544028de9ee8c04e
hermesxgjini <hermesxgjini@users.noreply.github.com>
2015-04-02 18:31:14 +0200

    Delete Navigating Back.cs

--- Navigating Back.cs
+++
@@ -1,8 +0,0 @@
-// Application ca execute logic to navigate back to precending page
-// Navigating back by using a button click event
-
-private void btnGoBack_Click(object sender, RoutedEventArgs e)
-{
-    if(this.Frame.CanGoBack)
-        this.Frame.GoBack();
-}
