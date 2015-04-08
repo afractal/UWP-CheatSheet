291405d71548ac1e7a1332e8a7de4f59a9f52e9f
hermesxgjini <hermesxgjini@users.noreply.github.com>
2015-04-08 18:07:14 +0200

    Update Mobile Service.cs

--- Mobile Service.cs
+++ Mobile Service.cs
@@ -2,19 +2,19 @@
 
 private async System.Threading.Tasks.Task InsertToDoItem()
 {
-  IMobileServiceTable<TodoItem> TodoTable = App.TaskMasterDemoClient.GetTable<TodoItem>();
+    IMobileServiceTable<TodoItem> TodoTable = App.TaskMasterDemoClient.GetTable<TodoItem>();
   
-  TodoItem t = new TodoItem();
-  t.Title = titleTextBox.Text;
-  t.Description = descriptionTextBox.Text;
-  t.DueDate = dueDatePicker.Date.ToString(9;
-  t.AssignedTo = assignesToTextBox.Text;
+    TodoItem t = new TodoItem();
+    t.Title = titleTextBox.Text;
+    t.Description = descriptionTextBox.Text;
+    t.DueDate = dueDatePicker.Date.ToString(9;
+    t.AssignedTo = assignesToTextBox.Text;
 
-  try
-  {
-    await TodoTable.InsertAsync(t);
-  }   
-  catch(Exception)
-  { // TODO: Insert error handling code}
+    try
+    {
+        await TodoTable.InsertAsync(t);
+    }   
+    catch(Exception)
+    { /* TODO: Insert error handling code*/ }
 }
 
