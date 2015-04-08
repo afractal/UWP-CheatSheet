e16efbe12e3d00a6bc03882a5d3f9545bc013d10
hermesxgjini <hermesxgjini@users.noreply.github.com>
2015-04-08 18:12:11 +0200

    Update Building a Data Smart Application.cs

--- Building a Data Smart Application.cs
+++ Building a Data Smart Application.cs
@@ -12,33 +12,24 @@
 ConnectionProfile internetConnectionProfile = NetworkInformation.GetInternetConnectionProfile();
 if(internetConnectionProfile != null)
 {
-  if(internetConnectionProfile.IsWlanConnectionProfile)
-  {
-    // connected on WiFi interface; double check it is not metered WiFi hotspot
-    ConnectionCost cc = internetConnectionProfile.GetConnectionCost();
-    if((NetworkCostType.Unknown == cc.NetworkCostType) || (NetworkCostType.Unrestricted == cc.NetworkCostType))
-    {
-      // assume free network; connect and start streaming content
-    }
-  } 
-
-  else if (internetConnectionProfile.IsWwanConnectionProfile)
-  {
-    ConnectionCost cc = InternetConnectionProfile.GetConnectionCost();
-    // check the type of data plan - make sure is not currently roaming
-    if(!=cc.Roaming)
-    {
-      // assume free network; connect and start streaming content
-    }
-    else if(NetworkCostType.Fixed == cc.NetworkCostType)
-    {
-      // make sure user not currently over limit or near limit
-      if ((!cc.OverDataLimit) && (!cc.ApproachingDataLimit))
-      {
-        // connect and start streaming content
-      }
-    }
-
-  }
+	if(internetConnectionProfile.IsWlanConnectionProfile)
+  	{
+    	// connected on WiFi interface; double check it is not metered WiFi hotspot
+    	ConnectionCost cc = internetConnectionProfile.GetConnectionCost();
+    	if((NetworkCostType.Unknown == cc.NetworkCostType) || (NetworkCostType.Unrestricted == cc.NetworkCostType)
+      	// assume free network; connect and start streaming content
+  	} 
+	else if (internetConnectionProfile.IsWwanConnectionProfile)
+  	{
+  		ConnectionCost cc = InternetConnectionProfile.GetConnectionCost();
+    	// check the type of data plan - make sure is not currently roaming
+    	if(!cc.Roaming)
+      	// assume free network; connect and start streaming content
+		else if(NetworkCostType.Fixed == cc.NetworkCostType)
+    	{
+      		// make sure user not currently over limit or near limit
+      		if ((!cc.OverDataLimit) && (!cc.ApproachingDataLimit))
+        	// connect and start streaming content
+    	}
+  	}
 }
-
