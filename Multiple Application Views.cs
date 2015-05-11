bbff125dc9f054b163263aef415a13ce195b77be
hermesxgjini <hermesxgjini@users.noreply.github.com>
2015-05-11 22:31:44 +0200

    Update Multiple Application Views.cs

--- Multiple Application Views.cs
+++ Multiple Application Views.cs
@@ -1,3 +1,7 @@
+// first you create a new view 
+// than you switch to the new view 
+// and finally you stay in the newly crated view
+
 Windows.ApplicationModel.Core.CoreApplication.CreateNewView
 Windows.UI.ViewManagement.ApplicationViewSwitcher.SwitchAsync
 Windows.UI.ViewManagement.ApplicationViewSwitcher.TryStandaloneAsync
